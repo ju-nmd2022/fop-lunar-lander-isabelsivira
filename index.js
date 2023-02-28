12865174484ab8bfb26e70e57b3ffdf4881ad614
isabelsivira <117159819+isabelsivira@users.noreply.github.com>
2023-02-28 22:53:55 +0100

    frog redo

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
-background(227, 249, 255);
+function setUp() {
+  createCanvas(630, 580);
+  background(197, 229, 232);
 
+  //floor
+  push();
+  noStroke();
+  fill(118, 184, 118);
+  rect(0, 480, width, 200);
+  pop();
+
+  //clouds
+  push();
+  noStroke();
+  fill(255, 255, 255);
+  ellipse(70, 62, 100);
+  ellipse(130, 62, 100);
+  ellipse(200, 62, 100);
+  ellipse(263, 162, 75);
+  ellipse(310, 162, 75);
+  ellipse(360, 162, 75);
+  pop();
+
+  //mr sun
+  push();
+  noStroke();
+  fill(250, 152, 60);
+  ellipse(560, 84, 250);
+  pop();
+
+  push();
+  noStroke();
+  fill(255, 241, 71);
+  ellipse(560, 84, 170);
+  pop();
+}
 //starting screen
 
 function startScreen() {
@@ -19,79 +53,8 @@ function startScreen() {
   }
 
   //blue flower
-}
-
-startScreen();
-
-//final screen
-
-function finalScreen() {}
-
-finalScreen();
-
-//game screen
-
-function gameScreen() {
-  //frog
-
-  function characterFrog() {
-    push();
-    noStroke();
-    fill(184, 247, 82);
-    ellipse(94, 300, 55);
-    ellipse(78, 278, 20);
-    ellipse(105, 276, 20);
-    pop();
-
-    //eyes
-
-    push();
-    noStroke();
-    fill(0, 0, 0);
-    ellipse(77, 275, 8);
-    ellipse(106, 273, 8);
-    pop();
-
-    //stomach
-
-    push();
-    noStroke();
-    fill(210, 250, 145);
-    ellipse(95, 307, 40);
-    pop();
-
-    //smile
-
-    push();
-    noFill();
-    beginShape();
-    vertex(87, 283);
-    bezierVertex(91, 287, 105, 283, 102, 282);
-    endShape();
-    pop();
-
-    //paws
-
-    push();
-    fill(184, 247, 82);
-    noStroke();
-    ellipse(101, 323, 15);
-    ellipse(89, 323, 15);
-    pop();
-  }
-
-  //move frog
-
-  push();
-  translate(15, 37);
-  characterFrog();
-  pop();
-
-  //blue flower
 
   function flowerBlue(x, y) {
-    //petals
-
     push();
     noStroke();
     fill(179, 211, 259);
@@ -102,8 +65,6 @@ function gameScreen() {
     ellipse(x - 59, y + 7, 20);
     pop();
 
-    //center
-
     push();
     noStroke();
     fill(252, 252, 121);
@@ -111,13 +72,9 @@ function gameScreen() {
     pop();
   }
 
-  flowerBlue(100, 150);
-
   //pink flower
 
   function flowerPink(x, y) {
-    //petals
-
     push();
     noStroke();
     fill(252, 111, 153);
@@ -128,8 +85,6 @@ function gameScreen() {
     ellipse(x - 59, y + 7, 20);
     pop();
 
-    //center
-
     push();
     noStroke();
     fill(252, 252, 121);
@@ -137,13 +92,9 @@ function gameScreen() {
     pop();
   }
 
-  flowerPink(300, 200);
-
   //purple flower
 
   function flowerPurple(x, y) {
-    //petals
-
     push();
     noStroke();
     fill(220, 130, 250);
@@ -154,8 +105,6 @@ function gameScreen() {
     ellipse(x - 59, y + 7, 20);
     pop();
 
-    //center
-
     push();
     noStroke();
     fill(252, 252, 121);
@@ -163,58 +112,184 @@ function gameScreen() {
     pop();
   }
 
-  flowerPurple(150, 200);
+  flowerBlue(100, 100);
+  flowerPink(150, 150);
+  flowerPurple(100, 200);
+  flowerBlue(600, 100);
+  flowerPink(550, 150);
+  flowerPurple(600, 200);
+}
 
-  //pillar drawing
+//startScreen();
+//QUESTION: how to make the button in the start screen work
 
-  function pillar(x, y, s) {
-    //bottom
+//game screen
+
+function gameScreen() {
+  //frog
+  setUp();
+  function characterFrog(x, y) {
+    push();
+    translate(x, y);
 
     push();
     noStroke();
-    fill(252, 218, 162);
-    rect(x - 100 * s, y - 78 * s, 30 * s, 300 * s);
+    fill(184, 247, 82);
+    ellipse(x + 100, y + 50, 60);
+    ellipse(x + 80, y + 20, 20);
+    ellipse(x + 120, y + 20, 20);
     pop();
 
-    //grass
-
     push();
     noStroke();
-    fill(69, 120, 56);
-    rect(x - 100 * s, y - 80 * s, 30 * s, 20 * s);
-    ellipse(x - 92 * s, y - 57 * s, 14 * s);
-    ellipse(x - 79 * s, y - 58 * s, 19 * s);
+    fill(0, 0, 0);
+    ellipse(x + 80, y + 20, 10);
+    ellipse(x + 120, y + 20, 10);
     pop();
 
-    //little details
-
     push();
     noStroke();
-    fill(244, 212, 252);
-    ellipse(x - 94 * s, y - 74 * s, 4 * s);
-    ellipse(x - 78 * s, y - 73 * s, 4 * s);
-    ellipse(x - 85 * s, y - 64 * s, 4 * s);
+    fill(210, 250, 145);
+    ellipse(x + 100, y + 55, 50);
+    pop();
+
+    push();
+    noFill();
+    beginShape();
+    vertex(x + 90, y + 34);
+    bezierVertex(x + 98, y + 45, x + 100, y + 45, x + 110, y + 34);
+    endShape();
+    pop();
+
+    push();
+    fill(184, 247, 82);
+    noStroke();
+    ellipse(x + 80, y + 50, 18);
+    ellipse(x + 120, y + 50, 18);
+    ellipse(x + 90, y + 75, 18);
+    ellipse(x + 110, y + 75, 18);
+    pop();
     pop();
   }
 
-  pillar(460, 350, 1.3);
+  characterFrog(100, 100);
+  //blue flower
 
-  //pillar animation
+  function flowerBlue(x, y, s) {
+    //petals
 
-  let x = 100;
+    push();
+    noStroke();
+    fill(179, 211, 259);
+    ellipse(x - 47 * s, y, 20 * s);
+    ellipse(x - 35 * s, y + 7 * s, 20 * s);
+    ellipse(x - 38 * s, y + 24 * s, 20 * s);
+    ellipse(x - 54 * s, y + 24 * s, 20 * s);
+    ellipse(x - 59 * s, y + 7 * s, 20 * s);
+    pop();
+
+    //center
+
+    push();
+    noStroke();
+    fill(252, 252, 121);
+    ellipse(x - 47 * s, y + 13 * s, 16 * s);
+    pop();
+  }
+
+  //pink flower
+
+  function flowerPink(x, y, s) {
+    //petals
+
+    push();
+    noStroke();
+    fill(252, 111, 153);
+    ellipse(x - 47 * s, y, 20 * s);
+    ellipse(x - 35 * s, y + 7 * s, 20 * s);
+    ellipse(x - 38 * s, y + 24 * s, 20 * s);
+    ellipse(x - 54 * s, y + 24 * s, 20 * s);
+    ellipse(x - 59 * s, y + 7 * s, 20 * s);
+    pop();
+
+    //center
+
+    push();
+    noStroke();
+    fill(252, 252, 121);
+    ellipse(x - 47 * s, y + 13 * s, 16 * s);
+    pop();
+  }
+
+  //purple flower
+
+  function flowerPurple(x, y, s) {
+    //petals
+
+    push();
+    noStroke();
+    fill(220, 130, 250);
+    ellipse(x - 47 * s, y, 20 * s);
+    ellipse(x - 35 * s, y + 7 * s, 20 * s);
+    ellipse(x - 38 * s, y + 24 * s, 20 * s);
+    ellipse(x - 54 * s, y + 24 * s, 20 * s);
+    ellipse(x - 59 * s, y + 7 * s, 20 * s);
+    pop();
+
+    //center
+
+    push();
+    noStroke();
+    fill(252, 252, 121);
+    ellipse(x - 47 * s, y + 13 * s, 16 * s);
+    pop();
+  }
+
+  let x = 200;
+  let y = 200;
+  const speed = 3;
 
   function draw() {
-    clear();
-    background(227, 249, 255);
-    characterFrog();
-    flowerBlue(100, 100);
-    flowerPink(150, 100);
-    pillar(x, 250, 1.0);
-    pillar(x - 120, 350, 1.0);
-    pillar(x - 260, 300, 1.0);
+    if (keyIsDown(65)) {
+      x = x - speed;
+    } else if (keyIsDown(68)) {
+      x = x + speed;
+    }
+    if (keyIsDown(87)) {
+      y = y - speed;
+    } else if (keyIsDown(83)) {
+      y = y + speed;
+    }
 
-    x = x + 2.6;
+    setUp();
+    flowerBlue(210, 475, 3.0);
+    flowerPink(450, 475, 3.0);
+    flowerPurple(680, 475, 3.0);
+    characterFrog(x, y, 0.5);
   }
 }
 
 //gameScreen();
+
+/*function loseScreen(){
+setUp();
+textSize(30);
+text("Too bad, you lost :(", 180, 265);
+stroke(0, 0, 0);
+noFill();
+rect(160, 205, 290, 100);
+
+}
+
+ loseScreen();*/
+
+function winScreen() {
+  setUp();
+  textSize(30);
+  text("Too bad, you lost :(", 180, 265);
+  stroke(0, 0, 0);
+  noFill();
+  rect(160, 205, 290, 100);
+}
+
+winScreen();
