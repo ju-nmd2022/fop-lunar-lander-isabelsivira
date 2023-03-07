09497e4a91cc6c1c8b6d770ff66afc2ec64290be
isabelsivira <117159819+isabelsivira@users.noreply.github.com>
2023-03-07 20:53:01 +0100

    it finally works

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function setUp() {
+function scenery() {
   createCanvas(630, 580);
   background(197, 229, 232);
 
@@ -35,79 +35,6 @@ function setUp() {
   pop();
 
   //blue flower
-
-  function flowerBlue(x, y, s) {
-    //petals
-
-    push();
-    noStroke();
-    fill(179, 211, 259);
-    ellipse(x - 47 * s, y, 20 * s);
-    ellipse(x - 35 * s, y + 7 * s, 20 * s);
-    ellipse(x - 38 * s, y + 24 * s, 20 * s);
-    ellipse(x - 54 * s, y + 24 * s, 20 * s);
-    ellipse(x - 59 * s, y + 7 * s, 20 * s);
-    pop();
-
-    //center
-
-    push();
-    noStroke();
-    fill(252, 252, 121);
-    ellipse(x - 47 * s, y + 13 * s, 16 * s);
-    pop();
-  }
-
-  //pink flower
-
-  function flowerPink(x, y, s) {
-    //petals
-
-    push();
-    noStroke();
-    fill(252, 111, 153);
-    ellipse(x - 47 * s, y, 20 * s);
-    ellipse(x - 35 * s, y + 7 * s, 20 * s);
-    ellipse(x - 38 * s, y + 24 * s, 20 * s);
-    ellipse(x - 54 * s, y + 24 * s, 20 * s);
-    ellipse(x - 59 * s, y + 7 * s, 20 * s);
-    pop();
-
-    //center
-
-    push();
-    noStroke();
-    fill(252, 252, 121);
-    ellipse(x - 47 * s, y + 13 * s, 16 * s);
-    pop();
-  }
-
-  //purple flower
-
-  function flowerPurple(x, y, s) {
-    //petals
-
-    push();
-    noStroke();
-    fill(220, 130, 250);
-    ellipse(x - 47 * s, y, 20 * s);
-    ellipse(x - 35 * s, y + 7 * s, 20 * s);
-    ellipse(x - 38 * s, y + 24 * s, 20 * s);
-    ellipse(x - 54 * s, y + 24 * s, 20 * s);
-    ellipse(x - 59 * s, y + 7 * s, 20 * s);
-    pop();
-
-    //center
-
-    push();
-    noStroke();
-    fill(252, 252, 121);
-    ellipse(x - 47 * s, y + 13 * s, 16 * s);
-    pop();
-  }
-  flowerBlue(210, 475, 3.0);
-  flowerPink(450, 475, 3.0);
-  flowerPurple(680, 475, 3.0);
 }
 //game screen
 
@@ -155,70 +82,164 @@ function characterFrog(x, y) {
   pop();
 }
 
+function flowerBlue(x, y, s) {
+  //petals
+
+  push();
+  noStroke();
+  fill(179, 211, 259);
+  ellipse(x - 47 * s, y, 20 * s);
+  ellipse(x - 35 * s, y + 7 * s, 20 * s);
+  ellipse(x - 38 * s, y + 24 * s, 20 * s);
+  ellipse(x - 54 * s, y + 24 * s, 20 * s);
+  ellipse(x - 59 * s, y + 7 * s, 20 * s);
+  pop();
+
+  //center
+
+  push();
+  noStroke();
+  fill(252, 252, 121);
+  ellipse(x - 47 * s, y + 13 * s, 16 * s);
+  pop();
+}
+
+//pink flower
+
+function flowerPink(x, y, s) {
+  //petals
+
+  push();
+  noStroke();
+  fill(252, 111, 153);
+  ellipse(x - 47 * s, y, 20 * s);
+  ellipse(x - 35 * s, y + 7 * s, 20 * s);
+  ellipse(x - 38 * s, y + 24 * s, 20 * s);
+  ellipse(x - 54 * s, y + 24 * s, 20 * s);
+  ellipse(x - 59 * s, y + 7 * s, 20 * s);
+  pop();
+
+  //center
+
+  push();
+  noStroke();
+  fill(252, 252, 121);
+  ellipse(x - 47 * s, y + 13 * s, 16 * s);
+  pop();
+}
+
+//purple flower
+
+function flowerPurple(x, y, s) {
+  //petals
+
+  push();
+  noStroke();
+  fill(220, 130, 250);
+  ellipse(x - 47 * s, y, 20 * s);
+  ellipse(x - 35 * s, y + 7 * s, 20 * s);
+  ellipse(x - 38 * s, y + 24 * s, 20 * s);
+  ellipse(x - 54 * s, y + 24 * s, 20 * s);
+  ellipse(x - 59 * s, y + 7 * s, 20 * s);
+  pop();
+
+  //center
+
+  push();
+  noStroke();
+  fill(252, 252, 121);
+  ellipse(x - 47 * s, y + 13 * s, 16 * s);
+  pop();
+}
+
 let characterFrogY = 100;
 let velocity = 2;
-let acceleration = 0.3;
-var mode;
+let acceleration = 0.5;
+let mode = 0;
 const speed = 2;
 let isGameActive = false;
+var move = 5;
 let x = 200;
 let y = 200;
 
 function draw() {
   clear();
-  setUp();
-  if (isGameActive == false) {
+  scenery();
+  flowerBlue(210, 475, 3.0);
+  flowerPink(450, 475, 3.0);
+  flowerPurple(680, 475, 3.0);
+
+  if (mode === 0) {
     textSize(30);
     text("Press enter to start", 190, 260);
   }
-  if (isGameActive) {
+  if (mode === 1) {
     characterFrogY = characterFrogY + velocity;
     velocity = velocity + acceleration;
-    if (keyIsDown(37)) {
+
+    if (keyIsDown(65)) {
       x = x - speed;
-    } else if (keyIsDown(39)) {
+    } else if (keyIsDown(68)) {
       x = x + speed;
     }
-    if (keyIsDown(38)) {
-      y = y - speed;
-    } else if (keyIsDown(40)) {
-      y = y + speed;
+
+    characterFrog(x, characterFrogY);
+
+    if (velocity > 13) {
+      mode = 2;
+      loseScreen();
     }
-    if (mouseIsPressed) {
+    if (characterFrogY > 480 && velocity < 10) {
+      winScreen();
+    }
+    if (keyIsDown(83)) {
       velocity = velocity - 1;
     }
-
-    if (characterFrogY > 400) {
-      mode = 0;
+    if (characterFrogY < -3) {
+      characterFrogY = characterFrogY + move;
     }
-    characterFrog(100, characterFrogY);
+    if (characterFrogY < -3) {
+      characterFrogY = characterFrogY - move;
+    }
   }
 }
-if (characterFrog(velocity < 3)) {
-  loseScreen();
+// the lines 198 to 214 were coded inspired by https://www.youtube.com/watch?v=z-4TeZpF8Aw&t=496s&ab_channel=CloudyHeavenGames
+function resetGame() {
+  // restart variables
+  characterFrogY = 100;
+  velocity = 2;
+  acceleration = 0.3;
+  mode = undefined;
+  x = 200;
+  y = 200;
+  mode = 0;
 }
 
 function keyPressed() {
   if (keyCode === 13) {
-    isGameActive = true;
+    if (mode === 0) {
+      mode = 1;
+    }
+  }
+  if (keyCode === 32) {
+    resetGame();
   }
 }
 
 //LOSING SCREEN
 
 function loseScreen() {
-  setUp();
+  scenery();
   textSize(30);
-  text("Too bad, you lost :(", 180, 265);
-  stroke(0, 0, 0);
-  noFill();
-  rect(160, 205, 290, 100);
+  text("Oops, you're going too fast!!", 180, 265);
+  textSize(20);
+  text("Press space to try again", 267, 300);
 }
 
 //WIN SCREEN
 
 function winScreen() {
-  setUp();
+  scenery();
   textSize(30);
   text("You won!! :D", 220, 265);
   stroke(0, 0, 0);
